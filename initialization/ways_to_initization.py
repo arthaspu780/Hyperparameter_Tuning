36286f04a6c82739b4fb7b6a7bed1d60ea3e5995
鹏帅 蒲 <764043233@qq.com>
2023-09-03 10:38:26 +0800

    add xavier_initial

--- initialization/ways_to_initization.py
+++ initialization/ways_to_initization.py
@@ -19,5 +19,15 @@ def initialize_parameters_he(layers_dims):
 layers_dims=[2,3,4]
 param=initialize_parameters_random(layers_dims)
 print(param)
+def initialize_parameters_xavier(layers_dims):
+    L = len(layers_dims)
+    parameter = {}
+    for i in range(1, L):
+        parameter["w" + str(i)] = np.random.randn(layers_dims[i], layers_dims[i - 1])*np.sqrt(1/layers_dims[i-1])
+        parameter["b" + str(i)] = np.zeros((layers_dims[i], 1))
+    return parameter
+layers_dims=[2,3,4]
+param=initialize_parameters_random(layers_dims)
+print(param)
 
 
